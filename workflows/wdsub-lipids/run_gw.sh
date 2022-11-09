1f5bc2f450e899e0a27170670f60b12b1af88208
Nils Hoffmann <3309580+nilshoffmann@users.noreply.github.com>
2022-11-09 15:36:21 +0100

    Use location of run_gw.sh as base path.

--- workflows/wdsub-lipids/run_gw.sh
+++ workflows/wdsub-lipids/run_gw.sh
@@ -1,9 +1,10 @@
 # -----------------------------------------------------------------------
 # CONFIGURATION VARIABLES
-shex_files_path='/home/ammar/Documents/github/biohackathon2022/workflows/wdsub-lipids';
+shex_files_path='wdsub-lipids';
 shex_file_names=(lipids);
-results_files_path='/home/ammar/Documents/github/biohackathon2022/workflows/wdsub-lipids/results';
-data_files_path='/home/ammar/Documents/github/biohackathon2022/workflows/wdsub-lipids/data';
+results_files_path='results';
+data_files_path='data';
+workdir=${pwd}
 # -----------------------------------------------------------------------
 # DO NOT MODIFY ANYTHING BELOW THIS LINE!!!
 
@@ -19,9 +20,9 @@ for shex_file_name in "${shex_file_names[@]}"
 do
     echo "Creating subset for ${shex_files_path}/${shex_file_name}.shex";
     dockerID=`docker run --rm --name wdsub -d \
-    	-v $data_files_path:/data \
-        -v $shex_files_path:/shex \
-        -v $results_files_path:/dumps \
+    	-v "$workdir/$data_files_path:/data" \
+        -v "$workdir/$shex_files_path:/shex" \
+        -v "$workdir/$results_files_path:/dumps" \
         wesogroup/wdsub:0.0.28 dump \
         -o /dumps/result_$shex_file_name.ttl.gz \
         -s /shex/$shex_file_name.shex \
